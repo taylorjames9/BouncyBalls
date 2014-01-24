ff1d35dffae039246656de4cd169d4a8696ee84f
taylorjames9 <taylorjames9@gmail.com>
2014-01-23 21:07:09 -0600

    Bubbles working with sound effects

---
+++ Assets/Scripts/CanonScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonScript : MonoBehaviour {
+
+
+	public GameObject thought;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			FireAThought ();
+		}
+	}
+
+	void FireAThought (){
+		GameObject aBulletThought = Instantiate (thought, transform.position, Quaternion.identity) as GameObject;
+		aBulletThought.rigidbody.AddForce(-5f,4f,0f,ForceMode.Force);
+		audio.Play();
+		while (aBulletThought.transform.localScale.x < 2.5) {
+			aBulletThought.transform.localScale += new Vector3 (0.005F, 0.005f, 0f);
+		}
+
+	}
+}
---
+++ Assets/Scripts/balloonCreator.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class balloonCreator : MonoBehaviour {
+
+
+	public GameObject prefaBalloon;
+	public int numBalloons;
+	List<GameObject> myListOfBalloons = new List<GameObject> ();
+	bool forceApplied = false;
+	bool balloonsGenerated = false;
+	private bool scaleMaxed = false;
+	public Vector3 spawnSpot;
+
+	// Use this for initialization
+	void Start () {
+
+		numBalloons = 20;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!balloonsGenerated) {
+			GenerateBalloons ();
+			balloonsGenerated = true;
+		}
+		GrowBalloon0 ();
+	}
+
+		void GenerateBalloons(){
+		for (int i=0; i <= numBalloons; i++) {
+			GameObject dizBalloon = (GameObject)Instantiate (prefaBalloon);
+			Vector3 spawnSpot = new Vector3 (5,0,0);
+
+			dizBalloon.transform.localPosition = spawnSpot;
+			dizBalloon.rigidbody.AddForce(-1,2,0,ForceMode.Force);
+			//dizBalloon.rigidbody.AddForce (10, 1, 0);
+			myListOfBalloons.Add (dizBalloon);
+		}
+	}
+	/*void FixedUpdate() {
+		if(!forceApplied){
+		for (int i = 0; i < numBalloons; i++) {
+				myListOfBalloons[i].rigidbody.AddForce (-1, 1, 0, ForceMode.Acceleration);
+		}
+		forceApplied = true; 
+	}
+	}*/
+
+	void GrowBalloon0(){
+		if(!scaleMaxed){
+			for (int i = 0; i <= numBalloons; i++) {
+				print ("We're in");
+				myListOfBalloons [i].transform.localScale += new Vector3 (0.05F, 0.05f, 0);
+				if (myListOfBalloons [1].transform.localScale.x >= 3) {
+					scaleMaxed = true;
+				}
+
+			}
+		}
+	}
+
+}
+
